bdd11364d202b1e868934896ec8bfc65db390cef
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2020-03-05 20:29:17 +0100

    omnify

--- omni_apollo.mk
+++ omni_apollo.mk
@@ -1,4 +1,3 @@
-#
 # Copyright 2014 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-add_lunch_combo omni_apollo-eng
-add_lunch_combo omni_apollo-userdebug
+# Inherit device parts
+$(call inherit-product, device/sony/apollo/aosp_h8314.mk)
+
+# Override Product Name
+PRODUCT_NAME := omni_apollo
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := apollo
